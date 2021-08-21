61122a796e720e8efab1246f8188678ce51df0e9
demirose41 <demirose41@gmail.com>
2021-08-20 20:48:14 -0700

    3. Built while loop that would run until there where no more pages to loop through, succesfully retrieving all quote information

--- scraping_project.py
+++ scraping_project.py
@@ -5,18 +5,19 @@ all_quotes =[]
 base_url = "http://quotes.toscrape.com"
 url = "/page/1"
 
+while url:
+    res = requests.get(f"{base_url}{url}")
+    print(f"Now Scrapping... {base_url}{url}.......")
+    soup = BeautifulSoup(res.text, 'html.parser')
+    quotes = soup.find_all(class_="quote")
 
+    for quote in quotes:
+        all_quotes.append({
+            "text" : quote.find(class_="text").get_text(),
+            "author" : quote.find(class_="author").get_text(),
+            "bio-link" :quote.find("a")["href"] 
+        })
+    next_button = soup.find(class_="next")
+    url = next_button.find('a')['href'] if next_button else None 
 
-res = requests.get(f"{base_url}{url}")
-soup = BeautifulSoup(res.text, 'html.parser')
-quotes = soup.find_all(class_="quote")
-for quote in quotes:
-    all_quotes.append({
-        "text" : quote.find(class_="text").get_text(),
-        "author" : quote.find(class_="author").get_text(),
-        "bio-link" :quote.find("a")["href"] 
-    })
-next_button = soup.find(class_="next")
-print(next_button.find('a')['href'])
-
-# print(all_quotes)
+print(all_quotes)
