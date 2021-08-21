439b3eac1f7046a17252f0375a8aabb8ab1053bd
demirose41 <demirose41@gmail.com>
2021-08-20 20:22:56 -0700

    1.inititial commit

---
+++ scraping_project.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get("http://quotes.toscrape.com")
+soup = BeautifulSoup(res.text)
+quotes = soup.find_all(class_="quote")
+for quote in quotes:
+    print(quote.find(class_="text").get_text())
